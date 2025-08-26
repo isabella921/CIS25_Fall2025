bdd95ec8878b2663a4b100ac7e119063c1504263
isabella921 <abetongi@gmail.com>
2025-08-25 20:14:25 -0700

    Add files via upload

---
+++ hello_inventory.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello Inventory System!\n\n";
+    cout << "\tData Types:\n"; 
+	cout << "\tSize of char: " << sizeof(char) << " bytes\n"; 
+    cout << "\tSize of int: " << sizeof(int) << " bytes\n"; 
+	cout << "\tSize of float: " << sizeof(float) << " bytes\n"; 
+
+}
